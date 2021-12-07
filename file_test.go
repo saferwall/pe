f13fbdc52db7506f11e4b08165c7d635cad78e9c
LordNoteworthy <lord.noteworthy@protonmail.com>
2021-12-07 12:45:12 +1100

    chore: add unit tests for authentihash and checksum calculation

--- file_test.go
+++ file_test.go
@@ -5,6 +5,7 @@
 package pe
 
 import (
+	"fmt"
 	"io/ioutil"
 	"path"
 	"path/filepath"
@@ -17,7 +18,7 @@ func getAbsoluteFilePath(testfile string) string {
 	return path.Join(filepath.Dir(p), testfile)
 }
 
-var tridtests = []struct {
+var peTests = []struct {
 	in  string
 	out error
 }{
@@ -25,16 +26,15 @@ var tridtests = []struct {
 }
 
 func TestParse(t *testing.T) {
-	for _, tt := range tridtests {
+	for _, tt := range peTests {
 		t.Run(tt.in, func(t *testing.T) {
-			filePath := tt.in
-			pe, err := New(filePath, nil)
+			file, err := New(tt.in, nil)
 			if err != nil {
 				t.Errorf("TestParse(%s) failed, reason: %v", tt.in, err)
 				return
 			}
 
-			got := pe.Parse()
+			got := file.Parse()
 			if got != nil {
 				t.Errorf("TestParse(%s) got %v, want %v", tt.in, got, tt.out)
 			}
@@ -43,20 +43,88 @@ func TestParse(t *testing.T) {
 }
 
 func TestNewBytes(t *testing.T) {
-	for _, tt := range tridtests {
+	for _, tt := range peTests {
 		t.Run(tt.in, func(t *testing.T) {
-			filePath := tt.in
-			data, err := ioutil.ReadFile(filePath)
-			pe, err := NewBytes(data, nil)
+			data, _ := ioutil.ReadFile(tt.in)
+			file, err := NewBytes(data, nil)
 			if err != nil {
-				t.Errorf("TestParse(%s) failed, reason: %v", tt.in, err)
+				t.Errorf("TestNewBytes(%s) failed, reason: %v", tt.in, err)
 				return
 			}
 
-			got := pe.Parse()
+			got := file.Parse()
 			if got != nil {
-				t.Errorf("TestParse(%s) got %v, want %v", tt.in, got, tt.out)
+				t.Errorf("TestNewBytes(%s) got %v, want %v", tt.in, got, tt.out)
 			}
+		})
+	}
+}
+
+func TestAuthentihash(t *testing.T) {
+
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{getAbsoluteFilePath("test/putty"),
+			"8be7d65593b0fff2e8b29004640261b8a0d4fcc651a14cd0b8b702b7928f8ee0"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			file, err := New(tt.in, nil)
+			if err != nil {
+				t.Errorf("TestAuthentihash(%s) failed, reason: %v", tt.in, err)
+				return
+			}
+			err = file.Parse()
+			if err != nil {
+				t.Errorf("TestAuthentihash(%s) failed, reason: %v", tt.in, err)
+				return
+			}
+
+			hash := file.Authentihash()
+			got := fmt.Sprintf("%x", hash)
+			if string(got) != tt.out {
+				t.Errorf("TestAuthentihash(%s) got %v, want %v", tt.in, got, tt.out)
+			}
+
+		})
+	}
+}
+
+func TestChecksum(t *testing.T) {
+
+	tests := []struct {
+		in  string
+		out uint32
+	}{
+		// file is DWORD aligned.
+		{getAbsoluteFilePath("test/putty"),
+			0x00122C22},
+		// file is not DWORD aligned and needs paddings.
+		{getAbsoluteFilePath("test/010001e68577ef704792448ff474d22c6545167231982447c568e55041169ef0"),
+			0x0006D558},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.in, func(t *testing.T) {
+			file, err := New(tt.in, nil)
+			if err != nil {
+				t.Errorf("TestChecksum(%s) failed, reason: %v", tt.in, err)
+				return
+			}
+			err = file.Parse()
+			if err != nil {
+				t.Errorf("TestChecksum(%s) failed, reason: %v", tt.in, err)
+				return
+			}
+
+			got := file.Checksum()
+			if got != tt.out {
+				t.Errorf("TestChecksum(%s) got %v, want %v", tt.in, got, tt.out)
+			}
+
 		})
 	}
 }
