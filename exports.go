82033ee5bfbd91c5423591a7bf33942ebc34d09c
LordNoteworthy <lord.noteworthy@protonmail.com>
2022-04-14 15:28:40 +1000

    fix: load configs GFIDS check exports

--- exports.go
+++ exports.go
@@ -315,9 +315,11 @@ func (pe *File) parseExportDirectory(rva, size uint32) error {
 
 // GetExportFunctionByRVA return an export function given an RVA.
 func (pe *File) GetExportFunctionByRVA(rva uint32) ExportFunction {
-	for _, exp := range pe.Export.Functions {
-		if exp.FunctionRVA == rva {
-			return exp
+	if pe.Export != nil {
+		for _, exp := range pe.Export.Functions {
+			if exp.FunctionRVA == rva {
+				return exp
+			}
 		}
 	}
 	return ExportFunction{}
