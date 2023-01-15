00dffe626ec3912f7f71d5c7ca02eb6221d7989e
LordNoteworthy <lord.noteworthy@protonmail.com>
2023-01-16 09:17:37 +1100

    pedumer to dump certificate directory

--- cmd/dump.go
+++ cmd/dump.go
@@ -222,9 +222,25 @@ func parsePE(filename string, cfg config) {
 	}
 
 	if cfg.wantCertificates && pe.FileInfo.HasSecurity {
-		fmt.Printf("\n\nSECURITY\n***********\n")
+		fmt.Printf("\nSECURITY\n*********\n")
 
+		cert := pe.Certificates
+		w := tabwriter.NewWriter(os.Stdout, 1, 1, 3, ' ', tabwriter.AlignRight)
+		fmt.Fprintln(w, "Length\tRevision\tCertificateType\t")
+		fmt.Fprintf(w, "0x%x\t0x%x\t0x%x\t\n", cert.Header.Length, cert.Header.Revision,
+			cert.Header.CertificateType)
+		w.Flush()
+		fmt.Print("\n   ---Raw Certificate dump---\n")
+		hexDump(cert.Raw)
+		fmt.Print("\n---Certificate ---\n\n")
+		fmt.Fprintf(w, "Issuer Name:\t %s\n", cert.Info.Issuer)
+		fmt.Fprintf(w, "Subject Name:\t %s\n", cert.Info.Subject)
+		fmt.Fprintf(w, "Serial Number:\t %x\n", cert.Info.SerialNumber)
+		fmt.Fprintf(w, "Validity From:\t %s to %s\n", cert.Info.NotBefore.String(), cert.Info.NotAfter.String())
+		fmt.Fprintf(w, "Signature Algorithm:\t %s\n", cert.Info.SignatureAlgorithm.String())
+		fmt.Fprintf(w, "PublicKey Algorithm:\t %s\n", cert.Info.PublicKeyAlgorithm.String())
+		w.Flush()
 	}
 
-	fmt.Println()
+	fmt.Print("\n")
 }
