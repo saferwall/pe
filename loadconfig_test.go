4cdbd0ea4ec8cba64b68769b14fd6eef0a0ffeba
LordNoteworthy <lord.noteworthy@protonmail.com>
2023-02-21 22:08:55 +1100

    ImageLoadConfigDirectory32v9 case

--- loadconfig_test.go
+++ loadconfig_test.go
@@ -17,14 +17,25 @@ func TestLoadConfigDirectory(t *testing.T) {
 		{
 			in: getAbsoluteFilePath("test/00da1a2a9d9ebf447508bf6550f05f466f8eabb4ed6c4f2a524c0769b2d75bc1"),
 			out: ImageLoadConfigDirectory32v3{
-				Size:                        92,
-				SecurityCookie:              4445800,
-				SEHandlerTable:              4430960,
-				SEHandlerCount:              37,
-				GuardCFCheckFunctionPointer: 4399712,
-				GuardCFFunctionTable:        4399828,
-				GuardCFFunctionCount:        144,
-				GuardFlags:                  268514560,
+				Size:                        0x5c,
+				SecurityCookie:              0x43D668,
+				SEHandlerTable:              0x439C70,
+				SEHandlerCount:              0x25,
+				GuardCFCheckFunctionPointer: 0x432260,
+				GuardCFFunctionTable:        0x4322D4,
+				GuardCFFunctionCount:        0x90,
+				GuardFlags:                  0x10013500,
+			},
+		},
+		{
+			in: getAbsoluteFilePath("test/3a081c7fe475ec68ed155c76d30cfddc4d41f7a09169810682d1c75421e98eaa"),
+			out: ImageLoadConfigDirectory32v9{
+				Size:                        0xa0,
+				SecurityCookie:              0x417008,
+				SEHandlerTable:              0x415410,
+				SEHandlerCount:              0x2,
+				GuardCFCheckFunctionPointer: 0x40e384,
+				GuardFlags:                  0x100,
 			},
 		},
 	}
