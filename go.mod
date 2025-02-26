4e5376d70f8ba0678e77ac6576c605ae5f3d65b8
dafenghuang <dafenghuang@tencent.com>
2025-02-26 11:01:20 +0800

    fix: support bip file

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.15
 
 require (
 	github.com/edsrzf/mmap-go v1.1.0
+	github.com/gabriel-vasile/mimetype v1.4.8
+	github.com/mat/besticon v3.12.0+incompatible
 	github.com/secDre4mer/pkcs7 v0.0.0-20240322103146-665324a4461d
-	golang.org/x/text v0.7.0
+	golang.org/x/image v0.24.0 // indirect
+	golang.org/x/text v0.22.0
 )
--- icon_test.go
+++ icon_test.go
@@ -1,6 +1,7 @@
 package pe
 
 import (
+	"bytes"
 	"os"
 	"testing"
 )
@@ -12,19 +13,46 @@ func Test_ParseIcon(t *testing.T) {
 		return
 	}
 
+	defer func() {
+		_ = file.Close()
+	}()
+
 	err = file.Parse()
 	if err != nil {
 		t.Errorf("parse file failed, err %v", err)
 		return
 	}
 
-	iconBytes, err := file.ParsePngIcon()
+	data, err := file.ParseIconToPng()
 	if err != nil {
-		t.Errorf("parse icon failed, err %v", err)
+		t.Errorf("parse file failed, err %v", err)
 		return
 	}
 
-	os.WriteFile("test.png", iconBytes, 0644)
+	_ = os.WriteFile("test.png", data, 0644)
+}
 
-	t.Logf("length %d", len(iconBytes))
+func TestBIPToPNGWithFile(t *testing.T) {
+	// 读取测试文件
+	bipData, err := os.ReadFile("temp.bip")
+	if err != nil {
+		t.Fatalf("fail to read bip file: %v", err)
+	}
+
+	// 验证BIP文件头
+	if !bytes.HasPrefix(bipData, []byte{0x28, 0x00, 0x00, 0x00}) {
+		t.Fatal("invalid bip header")
+	}
+
+	// 设置测试参数
+	const (
+		outputFile = "temp_test.png"
+	)
+
+	// 执行转换
+	if data, err := BIPToPNG(bipData); err != nil {
+		t.Fatalf("convert bip to png failed: %v", err)
+	} else {
+		_ = os.WriteFile(outputFile, data, 0644)
+	}
 }
